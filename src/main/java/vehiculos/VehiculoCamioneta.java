98f9050420f3a7821d0735564c18f7931d5a9445
Luis Monterroso <luismonterroso202031794@cunoc.edu.gt>
2021-06-24 00:12:41 -0600

    Camioneta creada

---
+++ src/main/java/vehiculos/VehiculoCamioneta.java
@@ -0,0 +1,25 @@
+package vehiculos;
+
+import interfaces.TransporteUrbano;
+
+public class VehiculoCamioneta extends Vehiculo implements TransporteUrbano {
+
+    public VehiculoCamioneta(boolean tipoDeCombustible, int capasidadDeGalones, int capasidadDePasajeros, double velocidadMaxima, double aceleracion) {
+        super(tipoDeCombustible, capasidadDeGalones, capasidadDePasajeros, velocidadMaxima, aceleracion);
+    }
+
+    @Override
+    public void girarALaIzquierda() {
+        System.out.println("Puedo girar a la izquierda");
+    }
+
+    @Override
+    public void girarALaDerecha() {
+        System.out.println("Puedo girar a la derecha");
+    }
+
+    @Override
+    public void transportarPersonas() {
+        System.out.println("Estoy transportando personas");
+    }
+}
